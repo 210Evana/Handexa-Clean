e674017df772b049a52bab44294fe7c2ad0a6b27
Yvonne <evanajuma2003@gmail.com>
2025-08-15 03:51:40 +0300

    err

--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -9,15 +9,13 @@ import {
 } from "../controllers/jobController.js";
 import { isAuthorized } from "../middlewares/auth.js";
 
-console.log("getAllJobs:", getAllJobs); // Debug log
-
 const router = express.Router();
 
-router.get("/getall", isAuthorized(["Job Seeker", "Employer"]), getAllJobs);
-router.post("/post", isAuthorized(["Employer"]), postJob);
-router.get("/getmyjobs", isAuthorized(["Employer"]), getMyJobs);
-router.put("/update/:id", isAuthorized(["Employer"]), updateJob);
-router.delete("/delete/:id", isAuthorized(["Employer"]), deleteJob);
-router.get("/:id", isAuthorized(["Job Seeker", "Employer"]), getSingleJob);
+router.get("/getall", isAuthorized, getAllJobs);
+router.post("/post", isAuthorized, postJob);
+router.get("/getmyjobs", isAuthorized, getMyJobs);
+router.put("/update/:id", isAuthorized, updateJob);
+router.delete("/delete/:id", isAuthorized, deleteJob);
+router.get("/:id", isAuthorized, getSingleJob);
 
 export default router;
